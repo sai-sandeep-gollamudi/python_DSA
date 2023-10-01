bf664960469ffaf3db4559c3c5483fe14f683a2d
sai-sandeep-gollamudi <107009354+sai-sandeep-gollamudi@users.noreply.github.com>
2023-09-30 23:19:31 -0500

    Add files via upload

---
+++ day10.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head=new_node
+        self.tail=new_node
+        self.length=1
+
+    def PrintList(self):
+        temp=self.head
+        while(temp!=None):
+            print(temp.value)
+            temp=temp.next
+
+ll = LinkedList(10)
+LinkedList.PrintList(ll)
+print(ll.head.value)
